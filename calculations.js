d6038663d0e135e2ae6eb74912bfc603e06439da
albi-kins <114768385+albi-kins@users.noreply.github.com>
2022-10-03 00:39:55 +0200

    Add files via upload

---
+++ calculations.js
@@ -0,0 +1,216 @@
+
+var objectHourlyPrices = {   
+
+    h00: {dayAndTime: "October 2, 2022, 00:00:00" , price: 350},
+    h01: {dayAndTime: "October 2, 2022, 01:00:00" , price: 350},
+    h02: {dayAndTime: "October 2, 2022, 02:00:00" , price: 350},
+    h03: {dayAndTime: "October 2, 2022, 03:00:00" , price: 350},
+    h04: {dayAndTime: "October 2, 2022, 04:00:00" , price: 350},
+    h05: {dayAndTime: "October 2, 2022, 05:00:00" , price: 380},
+    h06: {dayAndTime: "October 2, 2022, 06:00:00" , price: 400},
+    h07: {dayAndTime: "October 2, 2022, 07:00:00" , price: 420},
+    h08: {dayAndTime: "October 2, 2022, 08:00:00" , price: 450},
+    h09: {dayAndTime: "October 2, 2022, 09:00:00" , price: 450},
+    h10: {dayAndTime: "October 2, 2022, 10:00:00" , price: 450},
+    h11: {dayAndTime: "October 2, 2022, 11:00:00" , price: 450},
+    h12: {dayAndTime: "October 2, 2022, 12:00:00" , price: 450},
+    h13: {dayAndTime: "October 2, 2022, 13:00:00" , price: 450},
+    h14: {dayAndTime: "October 2, 2022, 14:00:00" , price: 450},
+    h15: {dayAndTime: "October 2, 2022, 15:00:00" , price: 450},
+    h16: {dayAndTime: "October 2, 2022, 16:00:00" , price: 450},
+    h17: {dayAndTime: "October 2, 2022, 17:00:00" , price: 450},
+    h18: {dayAndTime: "October 2, 2022, 18:00:00" , price: 450},
+    h19: {dayAndTime: "October 2, 2022, 19:00:00" , price: 420},
+    h20: {dayAndTime: "October 2, 2022, 20:00:00" , price: 400},
+    h21: {dayAndTime: "October 2, 2022, 21:00:00" , price: 380},
+    h22: {dayAndTime: "October 2, 2022, 22:00:00" , price: 350},
+    h23: {dayAndTime: "October 2, 2022, 23:00:00" , price: 350},
+                    
+    };
+
+console.log(objectHourlyPrices);
+
+var array15minPrices = [
+
+    {dayAndTime: "October 2, 2022, 00:00:00" , price: objectHourlyPrices.h00.price},
+    {dayAndTime: "October 2, 2022, 00:15:00" , price: objectHourlyPrices.h00.price},
+    {dayAndTime: "October 2, 2022, 00:30:00" , price: objectHourlyPrices.h00.price},
+    {dayAndTime: "October 2, 2022, 00:45:00" , price: objectHourlyPrices.h00.price},
+
+    {dayAndTime: "October 2, 2022, 01:00:00" , price: objectHourlyPrices.h01.price},
+    {dayAndTime: "October 2, 2022, 01:15:00" , price: objectHourlyPrices.h01.price},
+    {dayAndTime: "October 2, 2022, 01:30:00" , price: objectHourlyPrices.h01.price},
+    {dayAndTime: "October 2, 2022, 01:45:00" , price: objectHourlyPrices.h01.price},
+
+    {dayAndTime: "October 2, 2022, 02:00:00" , price: objectHourlyPrices.h02.price},
+    {dayAndTime: "October 2, 2022, 02:15:00" , price: objectHourlyPrices.h02.price},
+    {dayAndTime: "October 2, 2022, 02:30:00" , price: objectHourlyPrices.h02.price},
+    {dayAndTime: "October 2, 2022, 02:45:00" , price: objectHourlyPrices.h02.price},
+
+    {dayAndTime: "October 2, 2022, 03:00:00" , price: objectHourlyPrices.h03.price},
+    {dayAndTime: "October 2, 2022, 03:15:00" , price: objectHourlyPrices.h03.price},
+    {dayAndTime: "October 2, 2022, 03:30:00" , price: objectHourlyPrices.h03.price},
+    {dayAndTime: "October 2, 2022, 03:45:00" , price: objectHourlyPrices.h03.price},
+
+    {dayAndTime: "October 2, 2022, 04:00:00" , price: objectHourlyPrices.h04.price},
+    {dayAndTime: "October 2, 2022, 04:15:00" , price: objectHourlyPrices.h04.price},
+    {dayAndTime: "October 2, 2022, 04:30:00" , price: objectHourlyPrices.h04.price},
+    {dayAndTime: "October 2, 2022, 04:45:00" , price: objectHourlyPrices.h04.price},
+
+    {dayAndTime: "October 2, 2022, 05:00:00" , price: objectHourlyPrices.h05.price},
+    {dayAndTime: "October 2, 2022, 05:15:00" , price: objectHourlyPrices.h05.price},
+    {dayAndTime: "October 2, 2022, 05:30:00" , price: objectHourlyPrices.h05.price},
+    {dayAndTime: "October 2, 2022, 05:45:00" , price: objectHourlyPrices.h05.price},
+
+    {dayAndTime: "October 2, 2022, 06:00:00" , price: objectHourlyPrices.h06.price},
+    {dayAndTime: "October 2, 2022, 06:15:00" , price: objectHourlyPrices.h06.price},
+    {dayAndTime: "October 2, 2022, 06:30:00" , price: objectHourlyPrices.h06.price},
+    {dayAndTime: "October 2, 2022, 06:45:00" , price: objectHourlyPrices.h06.price},
+
+    {dayAndTime: "October 2, 2022, 07:00:00" , price: objectHourlyPrices.h07.price},
+    {dayAndTime: "October 2, 2022, 07:15:00" , price: objectHourlyPrices.h07.price},
+    {dayAndTime: "October 2, 2022, 07:30:00" , price: objectHourlyPrices.h07.price},
+    {dayAndTime: "October 2, 2022, 07:45:00" , price: objectHourlyPrices.h07.price},
+    
+    {dayAndTime: "October 2, 2022, 08:00:00" , price: objectHourlyPrices.h08.price},
+    {dayAndTime: "October 2, 2022, 08:15:00" , price: objectHourlyPrices.h08.price},
+    {dayAndTime: "October 2, 2022, 08:30:00" , price: objectHourlyPrices.h08.price},
+    {dayAndTime: "October 2, 2022, 08:45:00" , price: objectHourlyPrices.h08.price},
+    
+    {dayAndTime: "October 2, 2022, 09:00:00" , price: objectHourlyPrices.h09.price},
+    {dayAndTime: "October 2, 2022, 09:15:00" , price: objectHourlyPrices.h09.price},
+    {dayAndTime: "October 2, 2022, 09:30:00" , price: objectHourlyPrices.h09.price},
+    {dayAndTime: "October 2, 2022, 09:45:00" , price: objectHourlyPrices.h09.price},
+
+    {dayAndTime: "October 2, 2022, 10:00:00" , price: objectHourlyPrices.h10.price},
+    {dayAndTime: "October 2, 2022, 10:15:00" , price: objectHourlyPrices.h10.price},
+    {dayAndTime: "October 2, 2022, 10:30:00" , price: objectHourlyPrices.h10.price},
+    {dayAndTime: "October 2, 2022, 10:45:00" , price: objectHourlyPrices.h10.price},
+
+    {dayAndTime: "October 2, 2022, 11:00:00" , price: objectHourlyPrices.h11.price},
+    {dayAndTime: "October 2, 2022, 11:15:00" , price: objectHourlyPrices.h11.price},
+    {dayAndTime: "October 2, 2022, 11:30:00" , price: objectHourlyPrices.h11.price},
+    {dayAndTime: "October 2, 2022, 11:45:00" , price: objectHourlyPrices.h11.price},
+
+    {dayAndTime: "October 2, 2022, 12:00:00" , price: objectHourlyPrices.h12.price},
+    {dayAndTime: "October 2, 2022, 12:15:00" , price: objectHourlyPrices.h12.price},
+    {dayAndTime: "October 2, 2022, 12:30:00" , price: objectHourlyPrices.h12.price},
+    {dayAndTime: "October 2, 2022, 12:45:00" , price: objectHourlyPrices.h12.price},
+
+    {dayAndTime: "October 2, 2022, 13:00:00" , price: objectHourlyPrices.h13.price},
+    {dayAndTime: "October 2, 2022, 13:15:00" , price: objectHourlyPrices.h13.price},
+    {dayAndTime: "October 2, 2022, 13:30:00" , price: objectHourlyPrices.h13.price},
+    {dayAndTime: "October 2, 2022, 13:45:00" , price: objectHourlyPrices.h13.price},
+
+    {dayAndTime: "October 2, 2022, 14:00:00" , price: objectHourlyPrices.h14.price},
+    {dayAndTime: "October 2, 2022, 14:15:00" , price: objectHourlyPrices.h14.price},
+    {dayAndTime: "October 2, 2022, 14:30:00" , price: objectHourlyPrices.h14.price},
+    {dayAndTime: "October 2, 2022, 14:45:00" , price: objectHourlyPrices.h14.price},
+
+    {dayAndTime: "October 2, 2022, 15:00:00" , price: objectHourlyPrices.h15.price},
+    {dayAndTime: "October 2, 2022, 15:15:00" , price: objectHourlyPrices.h15.price},
+    {dayAndTime: "October 2, 2022, 15:30:00" , price: objectHourlyPrices.h15.price},
+    {dayAndTime: "October 2, 2022, 15:45:00" , price: objectHourlyPrices.h15.price},
+
+    {dayAndTime: "October 2, 2022, 16:00:00" , price: objectHourlyPrices.h16.price},
+    {dayAndTime: "October 2, 2022, 16:15:00" , price: objectHourlyPrices.h16.price},
+    {dayAndTime: "October 2, 2022, 16:30:00" , price: objectHourlyPrices.h16.price},
+    {dayAndTime: "October 2, 2022, 16:45:00" , price: objectHourlyPrices.h16.price},
+
+    {dayAndTime: "October 2, 2022, 17:00:00" , price: objectHourlyPrices.h17.price},
+    {dayAndTime: "October 2, 2022, 17:15:00" , price: objectHourlyPrices.h17.price},
+    {dayAndTime: "October 2, 2022, 17:30:00" , price: objectHourlyPrices.h17.price},
+    {dayAndTime: "October 2, 2022, 17:45:00" , price: objectHourlyPrices.h17.price},
+
+    {dayAndTime: "October 2, 2022, 18:00:00" , price: objectHourlyPrices.h18.price},
+    {dayAndTime: "October 2, 2022, 18:15:00" , price: objectHourlyPrices.h18.price},
+    {dayAndTime: "October 2, 2022, 18:30:00" , price: objectHourlyPrices.h18.price},
+    {dayAndTime: "October 2, 2022, 18:45:00" , price: objectHourlyPrices.h18.price},
+
+    {dayAndTime: "October 2, 2022, 19:00:00" , price: objectHourlyPrices.h19.price},
+    {dayAndTime: "October 2, 2022, 19:15:00" , price: objectHourlyPrices.h19.price},
+    {dayAndTime: "October 2, 2022, 19:30:00" , price: objectHourlyPrices.h19.price},
+    {dayAndTime: "October 2, 2022, 19:45:00" , price: objectHourlyPrices.h19.price},
+
+    {dayAndTime: "October 2, 2022, 20:00:00" , price: objectHourlyPrices.h20.price},
+    {dayAndTime: "October 2, 2022, 20:15:00" , price: objectHourlyPrices.h20.price},
+    {dayAndTime: "October 2, 2022, 20:30:00" , price: objectHourlyPrices.h20.price},
+    {dayAndTime: "October 2, 2022, 20:45:00" , price: objectHourlyPrices.h20.price},
+
+    {dayAndTime: "October 2, 2022, 21:00:00" , price: objectHourlyPrices.h21.price},
+    {dayAndTime: "October 2, 2022, 21:15:00" , price: objectHourlyPrices.h21.price},
+    {dayAndTime: "October 2, 2022, 21:30:00" , price: objectHourlyPrices.h21.price},
+    {dayAndTime: "October 2, 2022, 21:45:00" , price: objectHourlyPrices.h21.price},
+    
+    {dayAndTime: "October 2, 2022, 22:00:00" , price: objectHourlyPrices.h22.price},
+    {dayAndTime: "October 2, 2022, 22:15:00" , price: objectHourlyPrices.h22.price},
+    {dayAndTime: "October 2, 2022, 22:30:00" , price: objectHourlyPrices.h22.price},
+    {dayAndTime: "October 2, 2022, 22:45:00" , price: objectHourlyPrices.h22.price},
+
+    {dayAndTime: "October 2, 2022, 23:00:00" , price: objectHourlyPrices.h23.price},
+    {dayAndTime: "October 2, 2022, 23:15:00" , price: objectHourlyPrices.h23.price},
+    {dayAndTime: "October 2, 2022, 23:30:00" , price: objectHourlyPrices.h23.price},
+    {dayAndTime: "October 2, 2022, 23:45:00" , price: objectHourlyPrices.h23.price},
+
+    ];
+
+console.log(array15minPrices);
+
+
+function compute() {
+    
+    // inputs from user //
+    // inputs from user //
+
+    var applianceConsumption = sessionStorage.getItem("applianceConsumption");
+
+    var startDay = "October 2, 2022";
+    var startHour = sessionStorage.getItem("startHour");
+    var startMinute = "00"; // default value
+    var startSecond = "00"; // default value
+    var durationInHours = sessionStorage.getItem("durationInHours");
+
+
+    // data preparation//
+    // data preparation//
+
+    var startTime = startDay + ", " + startHour + ":" + startMinute + ":" + startSecond;
+    console.log(startTime);
+
+    var startTimeInMilliseconds = new Date(startTime).getTime();
+    var durationInMilliseconds = durationInHours * 60 * 60 * 1000;
+    var endTimeInMilliseconds = startTimeInMilliseconds + durationInMilliseconds;
+
+    var endTime = new Date (endTimeInMilliseconds);
+    console.log(endTime);
+
+    console.log(startTimeInMilliseconds);
+    console.log(endTimeInMilliseconds);
+
+
+    // data processing //
+    // data processing //
+
+    var costSum = 0;
+
+    array15minPrices.forEach(item =>{
+
+        if (new Date(item.dayAndTime).getTime() >= startTimeInMilliseconds && 
+            new Date(item.dayAndTime).getTime() < endTimeInMilliseconds){
+
+            costSum = costSum + applianceConsumption*item.price/4/1000/1000;
+
+            }
+        }
+    );
+
+    console.log(costSum);
+
+    var costTotal = Math.round(costSum*100)/100;
+
+    console.log(costTotal);
+
+    document.getElementById("costTotal").innerHTML = costTotal;
+
+    }
+
